a5cd81f37113e675273e71a780a8e8bc394305b6
BloodThi12sty <BloodThi12sty@410c5e10-1774-11df-8c82-d302e10fc01c>
2010-02-12 02:17:17 +0000

    added new ParallelSegmentation class

---
+++ trunk/cse160a3/src/ParallelSegmentation.java
@@ -0,0 +1,44 @@
+
+public class ParallelSegmentation extends Thread {
+
+	public ParallelSegmentation() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public ParallelSegmentation(Runnable target) {
+		super(target);
+		// TODO Auto-generated constructor stub
+	}
+
+	public ParallelSegmentation(String name) {
+		super(name);
+		// TODO Auto-generated constructor stub
+	}
+
+	public ParallelSegmentation(ThreadGroup group, Runnable target) {
+		super(group, target);
+		// TODO Auto-generated constructor stub
+	}
+
+	public ParallelSegmentation(ThreadGroup group, String name) {
+		super(group, name);
+		// TODO Auto-generated constructor stub
+	}
+
+	public ParallelSegmentation(Runnable target, String name) {
+		super(target, name);
+		// TODO Auto-generated constructor stub
+	}
+
+	public ParallelSegmentation(ThreadGroup group, Runnable target, String name) {
+		super(group, target, name);
+		// TODO Auto-generated constructor stub
+	}
+
+	public ParallelSegmentation(ThreadGroup group, Runnable target,
+			String name, long stackSize) {
+		super(group, target, name, stackSize);
+		// TODO Auto-generated constructor stub
+	}
+
+}
