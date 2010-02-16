c02d91918296ee7d36e3aaa9fbed47e5ec98a60c
codycoriva <codycoriva@410c5e10-1774-11df-8c82-d302e10fc01c>
2010-02-16 01:08:51 +0000

    Mostly worked on Master Thread(ParallelSegController).  Created threads, and joined threads.  Still have to do doSegmentation in ParallelSegmentation, and the reducePhase, which will reduce 2 threads(any cluster that go over a border between the 2 threads will be noticed to be the same cluster).

--- trunk/cse160a3/src/ParallelSegmentation.java
+++ trunk/cse160a3/src/ParallelSegmentation.java
@@ -1,44 +1,88 @@
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class ParallelSegmentation extends Thread {
+	
+	public static AtomicInteger numThreads;
+	public static ParallelSegmentation[] threads;
+	public static Semaphore arrival = new Semaphore(1);
+	public static Semaphore departure = new Semaphore(0);
+	public static int count = 0;
+	
+	private int tid;
+	private int[] labels;
+	private int[] imagePixels;
+	private int width;
+	private int lowHeight;//where in the image this will start.
+	private int highHeight;//where in the image this will end.
 
-	public ParallelSegmentation() {
-		// TODO Auto-generated constructor stub
+	public ParallelSegmentation(int tid) {
+		super();
+		this.tid = tid;
 	}
 
-	public ParallelSegmentation(Runnable target) {
-		super(target);
-		// TODO Auto-generated constructor stub
+	public void setLabels(int[] labels) {
+		this.labels = labels;
+		
 	}
 
-	public ParallelSegmentation(String name) {
-		super(name);
-		// TODO Auto-generated constructor stub
+	public void setImage(int[] imagePixels) {
+		this.imagePixels = imagePixels;
+		
 	}
 
-	public ParallelSegmentation(ThreadGroup group, Runnable target) {
-		super(group, target);
-		// TODO Auto-generated constructor stub
+	public void setWidth(int width) {
+		this.width = width;
+		
 	}
 
-	public ParallelSegmentation(ThreadGroup group, String name) {
-		super(group, name);
-		// TODO Auto-generated constructor stub
+	public void setHeightRange(int low, int high) {
+		this.lowHeight = low;
+		this.highHeight = high;
 	}
 
-	public ParallelSegmentation(Runnable target, String name) {
-		super(target, name);
-		// TODO Auto-generated constructor stub
+	private void reduce() throws InterruptedException{
+		barrier();//waits for all threads to finish before reducing.
+		if (numThreads.get() == 1)
+			return;
+		if (tid % 2 == 0 && tid + 1 == numThreads.get()){//if final thread unmatched.
+			threads[tid] = null;
+			tid = tid/2;//new index
+			while (threads[tid] != null){}//wait for the slot to be empty.
+			threads[tid] = this;
+		} else if(tid % 2 == 0){//even tids will combine the following odd tid into it.
+			highHeight = threads[tid+1].highHeight;
+			threads[tid] = null;
+			threads[tid+1] = null;
+			reducePhase();
+			tid = tid/2;//new index
+			while (threads[tid] != null){}//wait for the slot to be empty.
+			threads[tid] = this;
+		}
 	}
-
-	public ParallelSegmentation(ThreadGroup group, Runnable target, String name) {
-		super(group, target, name);
-		// TODO Auto-generated constructor stub
+	
+	/*
+	 * Reduce this threads portion of the image.
+	 */
+	private void reducePhase(){
+		//do later...........................
+		//....
 	}
-
-	public ParallelSegmentation(ThreadGroup group, Runnable target,
-			String name, long stackSize) {
-		super(group, target, name, stackSize);
-		// TODO Auto-generated constructor stub
+	
+	private void barrier() throws InterruptedException{
+		arrival.acquire();
+		count++;
+		if (count < numThreads.get())
+			arrival.release();
+		else
+			departure.release();
+		departure.acquire();
+		count--;
+		if (count > 0)
+			departure.release();
+		else
+			arrival.release();
+		return;
 	}
 
 }
