c5c20156692558a8d9c2ca3479b7846e2834fe45
bloodthi12sty <bloodthi12sty@410c5e10-1774-11df-8c82-d302e10fc01c>
2010-02-19 00:58:14 +0000

    blah blah

---
+++ trunk/cse160a3/src/ParallelSegController.java
@@ -0,0 +1,125 @@
+import java.awt.image.BufferedImage;
+import java.util.HashMap;
+import java.util.Random;
+
+
+public class ParallelSegController {
+	
+	public static int numThreads = 1;
+    public static int randomSeed = -1; // -1 = use time as seed
+
+    private int curNumThreads;
+    private ParallelSegmentation[] threads;
+    private BufferedImage segmentedImage; // output image
+    private int imagePixels[]; // Array of pixel values from input image, one int for each color/pixel.
+    private SegmentImg gui;
+    private int labels[];
+    private int width;
+    private int height;
+    private int pixelWidth;
+    
+    public ParallelSegController(){
+    }
+    
+    public void parallelSegInit(BufferedImage image, SegmentImg gui) {
+
+    	threads = new ParallelSegmentation[numThreads];
+    	// create representation of image
+    	width = image.getWidth();
+    	height = image.getHeight();
+    	segmentedImage = new BufferedImage(width, height, BufferedImage.TYPE_3BYTE_BGR);
+    	imagePixels = image.getRaster().getPixels(0, 0, width, height, (int[])null);
+    	this.gui = gui;
+
+       	// Space between two pixels in the imagePixel array.
+    	pixelWidth = image.getSampleModel().getNumDataElements();
+
+    	// Apply initial labels (each label is it's own index in the label array + 1)
+    	labels = new int[(width+32)*height];//padded, to avoid cache interference.
+    	
+    	for (int i = 0; i < (width+32)*height; i++) { 
+    		labels[i] = 0; 
+    	}
+
+    	// Label each pixel as a separate label.
+    	for (int i = 0; i < height; i++) {
+    		for (int j = 0; j < width; j++) {
+    		
+    			int idx = (i*(width+32) + j);
+    			int idx3 = idx*pixelWidth;
+    			
+    			// Comment this line if you want to label background pixels
+    			if (imagePixels[idx3] == 0) 
+    				continue;
+    			
+    			labels[idx] = idx+1;
+    			
+    		}
+    	}	
+    }
+    
+    public void doSegmentation(){
+    	ParallelSegmentation.numThreads.set(numThreads);
+    	ParallelSegmentation.threads = threads;
+    	for (curNumThreads = 0; curNumThreads < numThreads; curNumThreads++){
+    		ParallelSegmentation cur = new ParallelSegmentation(curNumThreads);
+    		cur.setLabels(labels);
+    		cur.setImage(imagePixels);
+    		cur.setWidth(width);
+    		cur.setHeightRange(curNumThreads*height/numThreads,
+    				(curNumThreads+1)*height/numThreads);
+    		cur.setPixelWidth(pixelWidth);
+    		cur.start();
+    		threads[curNumThreads] = cur;
+    	}
+    	try {threads[curNumThreads].join();}
+    	catch (InterruptedException e){
+    		System.err.println("Segmentation not able to complete due to interruption.");
+    		System.err.println(e.getStackTrace());
+    	}
+    }
+    
+    HashMap<Integer,Integer> red = new HashMap<Integer,Integer>();
+    HashMap<Integer,Integer> green = new HashMap<Integer,Integer>();
+    HashMap<Integer,Integer> blue = new HashMap<Integer,Integer>();
+
+    public BufferedImage getSegmentedImage() {
+
+
+        Random random = new Random();
+        // if -R <n> is supplied, use it as seed
+        if (randomSeed != -1) {
+            random.setSeed(Segmentation.randomSeed);
+        }
+
+        int array[] = new int[(width * height) * 3];
+
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                int label = labels[i*width+j];
+                if (label == 0) {
+                    red.put(label, 0);
+                    green.put(label, 0);
+                    blue.put(label, 0);
+                }
+
+                if (!red.containsKey(label)) {
+                    red.put(label, (int)(random.nextDouble()*255));
+                    green.put(label, (int)(random.nextDouble()*255));
+                    blue.put(label, (int)(random.nextDouble()*255));
+                }
+
+                array[(i*width+j)*3+0] = red.get(label);
+                array[(i*width+j)*3+1] = green.get(label);
+                array[(i*width+j)*3+2] = blue.get(label);
+            }
+        }
+ 
+        // Store pixels in BufferedImage
+        segmentedImage.getRaster().setPixels(0, 0, width, height, array);
+
+        return segmentedImage;
+    }
+
+    
+}
---
+++ trunk/cse160a3/src/ParallelSegmentation.java
@@ -0,0 +1,208 @@
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class ParallelSegmentation extends Thread {
+	
+	public static AtomicInteger numThreads;
+	public static ParallelSegmentation[] threads;
+	public static int threshold = 4;
+	public static Semaphore arrival = new Semaphore(1);
+	public static Semaphore departure = new Semaphore(0);
+	public static int count = 0;
+	
+	private int tid;
+	private int[] labels;
+	private int[] imagePixels;
+	private int width;
+	private int lowHeight;//where in the image this will start.
+	private int highHeight;//where in the image this will end.
+	private int pixelWidth;
+
+	public ParallelSegmentation(int tid) {
+		super();
+		this.tid = tid;
+	}
+
+	public void setLabels(int[] labels) {
+		this.labels = labels;
+		
+	}
+
+	public void setImage(int[] imagePixels) {
+		this.imagePixels = imagePixels;
+		
+	}
+
+	public void setWidth(int width) {
+		this.width = width;
+		
+	}
+
+	public void setHeightRange(int low, int high) {
+		this.lowHeight = low;
+		this.highHeight = high;
+	}
+	
+	public void setPixelWidth(int width){
+		pixelWidth = width;
+	}
+
+	public void run() {
+		doSegmentation();
+		try {
+			reduce();
+		} catch (InterruptedException e) {
+			System.out.print("Reduction distrupted in thread ");
+			System.out.println(tid);
+			e.printStackTrace();
+		}
+	}
+	
+	public void doSegmentation(){
+
+        int pix[] = imagePixels;
+        int maxN = Math.max(width, highHeight-lowHeight);
+
+        int phases = (int) Math.ceil(Math.log(maxN) / Math.log(2)) + 1;
+        System.out.print("Ok, " + (phases+1) + " phases scheduled for");
+        System.out.println(" thread " + tid);
+
+        for (int pp = 0; pp <= phases; pp++) {
+
+            // pass one. Find neighbors with better labels.
+            for (int i = highHeight - 1; i >= lowHeight; i--) {
+                for (int j = width - 1; j >= 0; j--) {
+                    int idx = i*width + j;
+                    int idx3 = idx*pixelWidth;
+
+                    if (labels[idx] == 0) 
+                    	continue;
+
+                    int ll = labels[idx]; // save previous label
+
+                    // pixels are stored as 3 ints in "pix" array. we just use the first of them. 
+                    // Compare with each neighbor
+                    if (i != highHeight - 1 && 
+                            Math.abs(pix[((i+1)*width + j)*pixelWidth] - pix[idx3]) < threshold) 
+                        labels[idx] = Math.max(labels[idx], labels[(i+1)*width + j]);
+
+                    if (i != lowHeight && 
+                            Math.abs(pix[((i-1)*width + j)*pixelWidth] - pix[idx3]) < threshold) 
+                        labels[idx] = Math.max(labels[idx], labels[(i-1)*width + j]);
+
+                    if (i != highHeight - 1 && j != width - 1 && 
+                            Math.abs(pix[((i+1)*width + j + 1)*pixelWidth] - pix[idx3]) < threshold) 
+                        labels[idx] = Math.max(labels[idx], labels[(i+1) * width + j + 1]);
+
+                    if (i != lowHeight && j != width - 1 && 
+                            Math.abs(pix[((i-1) * width + j + 1)*pixelWidth] - pix[idx3]) < threshold) 
+                        labels[idx] = Math.max(labels[idx], labels[(i-1) * width + j + 1]);
+
+                    if (i != highHeight - 1 && j != 0 && 
+                            Math.abs(pix[((i+1) * width + j - 1)*pixelWidth] - pix[idx3]) < threshold) 
+                        labels[idx] = Math.max(labels[idx], labels[(i+1) * width + j - 1]);
+
+                    if (i != lowHeight && j != 0 && 
+                            Math.abs(pix[((i-1) * width + j - 1)*pixelWidth] - pix[idx3]) < threshold) 
+                        labels[idx] = Math.max(labels[idx], labels[(i-1) * width + j - 1]);
+
+                    if (j != 0 && 
+                            Math.abs(pix[(i*width + j - 1)*pixelWidth] - pix[idx3]) < threshold)
+                        labels[idx] = Math.max(labels[idx], labels[i*width + j - 1]);
+
+                    if (j != width - 1 && 
+                            Math.abs(pix[(i*width + j + 1)*pixelWidth] - pix[idx3]) < threshold)
+                        labels[idx] = Math.max(labels[idx], labels[i*width + j + 1]);
+
+                    // if label assigned to this pixel during "follow the pointers" step is worse than label
+                    // of one of its neighbors, then that means that we're converging to local maximum instead
+                    // of global one. To correct this, we replace our root pixel's label with better newly found one.
+                    if (ll < labels[idx]) {
+                        if (labels[ll - 1] < labels[idx])
+                            labels[ll - 1] = labels[idx];
+                    }
+                }
+            }
+
+            // pass two. propagates the updated label of the parent to the children.
+            for (int i = 0; i < highHeight; i++) {
+                for (int j = 0; j < width; j++) {
+                    int idx = i*width + j;
+                    if (labels[idx] != 0) {
+                        labels[idx] = Math.max(labels[idx], labels[labels[idx] - 1]); 
+                        // subtract 1 from pixel's label to convert it to array index
+                    }
+                }
+            }
+
+            System.out.println( "Phase " + (pp) + " done in thread" + tid);
+
+            // (Updates the GUI after each phase. Slow, useful for visual debugging)
+            // comment it out for parallel version
+            //gui.updateTimer(pp);
+            // comment it out for parallel version
+            //gui.updateSegmentedImage(getSegmentedImage(), false); 
+        }
+        
+        // comment it out for parallel version
+        //gui.updateDoneLabel();
+    }
+
+	
+	private void reduce() throws InterruptedException{
+		barrier();//waits for all threads to finish before reducing.
+		boolean reduces = false;
+		int newHigh = -1;
+		if (numThreads.get() == 1){
+			numThreads.set(0);
+			return;}//returns if there is only one thread left.
+		if (tid % 2 == 0 && tid + 1 == numThreads.get()){//if final thread unmatched.
+			threads[tid] = null;
+			tid = tid/2;//new index
+			while (threads[tid] != null){}//wait for the slot to be empty.
+			threads[tid] = this;
+		} else if(tid % 2 == 0){//even tids will combine the following odd tid into it.
+			reduces = true;
+			newHigh = threads[tid+1].highHeight;
+			threads[tid] = null;
+			threads[tid+1] = null;
+			reducePhase();
+			highHeight = newHigh;
+			tid = tid/2;//new index
+			while (threads[tid] != null){}//wait for the slot to be empty.
+			threads[tid] = this;
+		}
+		barrier();//need all threads to be reduced
+		if (this != threads[tid])//this has been merged.
+			return;
+		if (reduces){
+			numThreads.decrementAndGet();
+		}
+		reduce();//until there is only one thread left.
+	}
+	
+	/*
+	 * Reduce this threads portion of the image.
+	 */
+	private void reducePhase(){
+		//do later...........................
+		//....
+	}
+	
+	private void barrier() throws InterruptedException{
+		arrival.acquire();
+		count++;
+		if (count < numThreads.get())
+			arrival.release();
+		else
+			departure.release();
+		departure.acquire();
+		count--;
+		if (count > 0)
+			departure.release();
+		else
+			arrival.release();
+		return;
+	}
+
+}
